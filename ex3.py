8d5cdb93a33f39044298e81cffbff6099eae2576
Deeraj26 <rajini_kr@yahoo.com>
2014-09-06 12:33:23 -0500

    fucntion changes

--- ex3.py
+++ ex3.py
@@ -5,18 +5,31 @@ import thinkstats
 import survey
 import Pmf
 from collections import defaultdict
+import collections
 
-pump_wt=[1,1,1,3,3,591]
-hist=Pmf.MakeHistFromList(pump_wt)
-print hist
-print hist.Freq(1)
-print pump_wt
+#hist=Pmf.MakeHistFromList(pump_wt)
+#print hist
+#print hist.Freq(1)
+#print pump_wt
 
-hist_dict = {}
-for val in sorted(hist.Values()): 
-            hist_dict[val] = hist.Freq(val)
-print hist_dict
-print type(hist_dict)
+#hist_dict = {}
+#for val in sorted(hist.Values()): 
+#            hist_dict[val] = hist.Freq(val)
+#print hist_dict
+#print type(hist_dict)
 
-print hist_dict.keys
-print hist_dict.values
+
+def _mode(myList):
+    table = collections.Counter(iter(myList)).most_common()
+    print table
+    print 'The mode is', table[0][0]
+    print 'Frequency of the Mode', table[0][1]
+
+
+
+if __name__ == '__main__':
+      myList =[1,1,1,3,3,591]
+      print "Mode (with my PmfMean func.): ", _mode(myList)
+    
+
+
