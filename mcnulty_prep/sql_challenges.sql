fb83e0bd90a96013544131d56d106b84def8defa
Deeraj26 <rajini_kr@yahoo.com>
2014-09-24 19:15:17 -0400

    modified:   mcnulty_prep/sql_challenges.sql

--- mcnulty_prep/sql_challenges.sql
+++ mcnulty_prep/sql_challenges.sql
@@ -1,4 +1,4 @@
-Challenge1: 
+/*Challenge1: 
 Who has played the most matches total in all of US Open, AUST Open, French Open?  Answer this both for men and women.
 MEN: Rafael Nadal with 21 matches
 | Richard Gasquet         |            13 |
@@ -9,6 +9,7 @@ MEN: Rafael Nadal with 21 matches
 | David Ferrer            |            17 |
 | Rafael Nadal            |            21 |
 +-------------------------+---------------+
+*/
 
     select player, sum(count) as number_of_matches from (select player1 as player, count(*) as count from 
  	aus_men_2013 group by player1 
@@ -18,7 +19,7 @@ MEN: Rafael Nadal with 21 matches
  	UNION ALL select player1 as player, count(*) as count from us_men_2013 group by player1  	
  	UNION ALL select player2 as player, count(*) as count from us_men_2013 group by player2 ) AS temporary_table group by player 
  	order by number_of_matches desc LIMIT 5;
-
+/*
   Angelique Kerber           |             8 |
 | Eugenie Bouchard           |             8 |
 | Sloane Stephens            |             8 |
@@ -31,7 +32,8 @@ MEN: Rafael Nadal with 21 matches
 | Serena Williams            |            11 |
 | Maria Sharapova            |            11 |
 +----------------------------+---------------+
-WOMEN: Serena Williams, Maria Sharapova, Agnieszka Radwanska, & Victoria Azarenka with 11 matches
+WOMEN: Serena Williams, Maria Sharapova, Agnieszka Radwanska, & Victoria Azarenka with 11 matches*/
+
 select player, sum(count) as number_of_matches from
  	(select player1 as player, count(*) as  count from aus_women_2013 group by player1
  	UNION ALL select player2 as player, count(*) as count from aus_women_2013 group by player2 
@@ -41,7 +43,7 @@ select player, sum(count) as number_of_matches from
  	UNION ALL select player2 as player, count(*) as count from us_women_2013 group by player2) AS 
  	temporary_table group by player order by number_of_matches desc LIMIT 5;
 
-Challenge 2:
+/*Challenge 2:
  Who has the highest first serve percentage? (Just the maximum value in a single match)
 | Carlos Berlocq          | 84                |
 | Rafael Nadal            | 84                |
@@ -50,7 +52,8 @@ Challenge 2:
 
 MEN: Nadal,
      Monfils, and 
-     Berlocq with 84%
+     Berlocq with 84% */
+
 select player, max(max_serve_p) as maxfsp from (select player1 as player, 
  	max(FSP_1) as max_serve_p from aus_men_2013 group by player1    
  	UNION ALL select player2 as player, max(FSP_2) as max_serve_p  from aus_men_2013  
@@ -64,7 +67,7 @@ select player, max(max_serve_p) as maxfsp from (select player1 as player,
  	UNION ALL select player2 as player, max(FSP_2) as max_serve_p  from us_men_2013  
  	group by player2) AS temporary_table group by player order by maxfsp desc LIMIT 5;
 
-WOMEN:  S. Errani with 93%
+/*WOMEN:  S. Errani with 93%*/
 select player, max(max_serve_p) as maxfsp from (select player1 as player, 
  	max(FSP_1) as max_serve_p from aus_women_2013 group by player1    
  	UNION ALL select player2 as player, max(FSP_2) as max_serve_p  from 
@@ -80,13 +83,14 @@ select player, max(max_serve_p) as maxfsp from (select player1 as player,
  	desc LIMIT 5;
 
 
-Challenge 3:
+/*Challenge 3:
 What are the unforced error percentages of the top three players with the most wins?
 
  TOP 3 MEN WITH MOST WINS:
  Rafael Nadal (21.2%),
  Stanislas Wawrinka (23.8%),
- David Ferrer (21.6%),
+ David Ferrer (21.6%),*/
+
 select player, sum(wins) as Total_Wins from (select player1 as player, count(*) as wins 
  	from aus_men_2013 where result=1 group by player1    
  	UNION ALL select player2 as player, count(*) as wins from aus_men_2013 where result=0 group by 
@@ -122,10 +126,10 @@ select player, sum(UFE) as SUM_UNFORCED_ERRORS, sum(total_points) as SUM_TOTAL_P
 
 
 
-TOP 3 WOMEN WITH MOST WINS: 
+/*TOP 3 WOMEN WITH MOST WINS: 
 Serena Williams (43.2%), 
 Victoria Azarenka (38.9%), 
-Agnieszka Radwanska (27.5%)
+Agnieszka Radwanska (27.5%)*/
 
 
 select player, sum(wins) as Total_Wins from (select player1 as player, count(*) as wins 
